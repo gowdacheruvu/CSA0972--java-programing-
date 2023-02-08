08f890ac4717f5599282fc34f1870d6427c2c838
gowdacheruvu <113328152+gowdacheruvu@users.noreply.github.com>
2023-02-08 10:28:39 +0530

    Create numbers.java

---
+++ numbers.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class numbers
+{
+  public static void main(String[] args)
+  {
+    int m,n,k;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter m, n and k values");
+    m=sc.nextInt();
+    n=sc.nextInt();
+    k=sc.nextInt();
+    for(int i=m+(k-1);i<=n;i++)
+     {
+       System.out.print(m);
+  
+     }
+   }
+}
